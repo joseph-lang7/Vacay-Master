367bbc1fe1be406084ead1c47b043d7092f04b42
joseph <langj6757@gmail.com>
2024-05-03 15:49:14 -0700

    Setup auth pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { HomePage, RegisterPage, LoginPage } from "./pages";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

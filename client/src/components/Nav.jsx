2f0e2f77536e45d71a558baf6c9d96ad3796f9c0
joseph <langj6757@gmail.com>
2024-05-04 20:27:02 -0700

    Implement dropdown for authenticated and unauthenticated users

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import profileIcon from "/profileIcon.svg";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const Nav = () => {
   const [dropdown, setDropdown] = useState(false);
-  const dropdownItems = [
+  const user = useSelector((state) => state.user);
+  const authenticatedDropdownItems = [
     { label: "Trip List", href: "1" },
     { label: "Wish List", href: "2" },
     { label: "Property List", href: "3" },
     { label: "Reservation List", href: "4" },
+  ];
+  const unauthenticatedDropdownItems = [
+    { label: "Login", href: "/login" },
+    { label: "Register", href: "/register" },
   ];
   return (
     <nav className="w-screen h-20 bg-slate-700 flex items-center px-7 justify-between">
@@ -18,11 +25,24 @@ const Nav = () => {
         className="w-16 bg-white h-10 rounded-full flex justify-center items-center "
       >
         <img src={profileIcon} className="w-[40px] h-[40px]" />
-        {dropdown && (
+        {dropdown && user && (
           <div className="absolute bg-slate-700 text-white h-auto p-5 w-auto  translate-y-36">
             <ul className="flex flex-col gap-4">
-              {dropdownItems.map((dropdownItem) => (
-                <li key={dropdownItem.href}>{dropdownItem.label}</li>
+              {authenticatedDropdownItems.map((dropdownItem) => (
+                <li key={dropdownItem.href}>
+                  <Link to={dropdownItem.href}>{dropdownItem.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {dropdown && !user && (
+          <div className="absolute bg-slate-700 text-white h-auto p-5 w-auto  translate-y-20">
+            <ul className="flex flex-col gap-4">
+              {unauthenticatedDropdownItems.map((dropdownItem) => (
+                <li key={dropdownItem.href}>
+                  <Link to={dropdownItem.href}>{dropdownItem.label}</Link>
+                </li>
               ))}
             </ul>
           </div>

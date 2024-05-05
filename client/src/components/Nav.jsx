573f5cd99ff7c0ad679464818df2af94999f6f7d
joseph <langj6757@gmail.com>
2024-05-04 21:07:09 -0700

    Implement logout functionality

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,9 @@
 import profileIcon from "/profileIcon.svg";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import menu from "/menu.svg";
+import { setLogout } from "../redux/state";
 const Nav = () => {
   const [dropdown, setDropdown] = useState(false);
   const user = useSelector((state) => state.user);
@@ -15,6 +17,7 @@ const Nav = () => {
     { label: "Login", href: "/login" },
     { label: "Register", href: "/register" },
   ];
+  const dispatch = useDispatch();
   return (
     <nav className="w-screen h-20 bg-slate-700 flex items-center px-7 justify-between">
       <div className="text-white text-2xl">
@@ -22,17 +25,37 @@ const Nav = () => {
       </div>
       <button
         onClick={() => setDropdown(!dropdown)}
-        className="w-16 bg-white h-10 rounded-full flex justify-center items-center "
+        className="w-20 bg-white h-12 rounded-full flex items-center gap-2 justify-center"
       >
-        <img src={profileIcon} className="w-[40px] h-[40px]" />
+        {!user && (
+          <img
+            src={profileIcon}
+            className="w-[45px] h-[45px] rounded-full"
+            alt="Profile photo"
+          />
+        )}
+        {user && (
+          <img
+            src={`http://localhost:3001/${user.profileImagePath.replace(
+              "public",
+              ""
+            )}`}
+            alt="Profile photo"
+            className="w-[45px] h-[45px] rounded-full object-cover"
+          />
+        )}
+        <img src={menu} className="w-[25px] h-[25px]" alt="Menu icon" />
         {dropdown && user && (
-          <div className="absolute bg-slate-700 text-white h-auto p-5 w-auto  translate-y-36">
+          <div className="absolute bg-slate-700 text-white h-auto p-5 w-auto  translate-y-40">
             <ul className="flex flex-col gap-4">
               {authenticatedDropdownItems.map((dropdownItem) => (
                 <li key={dropdownItem.href}>
                   <Link to={dropdownItem.href}>{dropdownItem.label}</Link>
                 </li>
               ))}
+              <li key="logout" onClick={() => dispatch(setLogout())}>
+                <Link to="/login">Logout</Link>
+              </li>
             </ul>
           </div>
         )}

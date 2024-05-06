60dfa5b1ec2f79bac0a2866d33d391c27c9c186d
joseph <langj6757@gmail.com>
2024-05-05 17:38:40 -0700

    Style destination cards

--- client/src/components/DestinationCard.jsx
+++ client/src/components/DestinationCard.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 const DestinationCard = ({ imagePath, title }) => {
   return (
-    <div
-      className="bg-cover bg-center h-[350px] w-[350px]"
-      style={{ backgroundImage: `url("/${imagePath}")` }}
-    >
-      <h2>{title}</h2>
-    </div>
+    <Link className="" to="/">
+      <div
+        className="bg-cover bg-center h-[350px] w-[350px] hover:scale-110 transition-all duration-300 hover:shadow-lg flex justify-center items-center"
+        style={{ backgroundImage: `url("/${imagePath}")` }}
+      >
+        <h2 className="text-white text-2xl">{title}</h2>
+      </div>
+    </Link>
   );
 };
 
--- client/src/components/ExploreDestinations.jsx
+++ client/src/components/ExploreDestinations.jsx
@@ -5,11 +5,15 @@ const ExploreDestinations = () => {
     <div className="mt-12 flex justify-center items-center">
       <div className=" max-w-[1500px] flex flex-col justify-center items-center">
         <h2 className="text-center text-4xl mb-8">Explore Destinations</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-3">
-          <DestinationCard title="Card" imagePath="hero-bg.png" />
-          <DestinationCard title="Card" imagePath="hero-bg.png" />
-          <DestinationCard title="Card" imagePath="hero-bg.png" />
-          <DestinationCard title="Card" imagePath="hero-bg.png" />
+        <p className="text-xl mb-8 text-center px-2">
+          Explore our wide range of vacation destination to find the perfect
+          spot for your next vacation.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-5">
+          <DestinationCard title="Beach" imagePath="beach.png" />
+          <DestinationCard title="Country" imagePath="country.png" />
+          <DestinationCard title="Island" imagePath="island.png" />
+          <DestinationCard title="City" imagePath="city.png" />
         </div>
       </div>
     </div>
